5b9eff2c911c64e77bc2a0583e10f3a5c5a13c77
Harish <arishh2@gmail.com>
2019-06-25 23:26:55 +0530

    Main ANimation and MObile Chat UI fixed

--- app.js
+++ app.js
@@ -7,124 +7,135 @@ express.set("view engine", "ejs");
 express.use(expresss.static("assets"));
 
 express.get("/", function(req, res) {
-  res.render("index");
+    res.render("index");
 });
+
 //Server Setup
-var server = express.listen(process.env.PORT, process.env.IP, function() {
-  console.log("THe Server is running");
-});
+if (process.env.PORT) {
+    var server = express.listen(process.env.PORT, process.env.IP, function() {
+        var server = express.listen(process.env.PORT, process.env.IP, function() {
+            console.log("THe Server is running");
+        });
+    });
+} else {
+    var server = express.listen(3000, function() {
+        console.log("THe Server is running");
+    });
+}
 
 //Socket Setup
 var io = socket(server);
 //GAME VARIABLES
 var choice1 = "",
-  choice2 = "";
+    choice2 = "";
 
 //FUNCTIONS
 
 //Function to calculate winner
 function getWinner(p, c) {
-  if (p === c) {
-    return "draw";
-  } else if (p === "rock") {
-    if (c === "paper") {
-      return "2";
-    } else {
-      return "1";
+    if (p === c) {
+        return "draw";
+    } else if (p === "rock") {
+        if (c === "paper") {
+            return "2";
+        } else {
+            return "1";
+        }
+    } else if (p === "paper") {
+        if (c === "scissors") {
+            return "2";
+        } else {
+            return "1";
+        }
+    } else if (p === "scissors") {
+        if (c === "rock") {
+            return "2";
+        } else {
+            return "1";
+        }
     }
-  } else if (p === "paper") {
-    if (c === "scissors") {
-      return "2";
-    } else {
-      return "1";
-    }
-  } else if (p === "scissors") {
-    if (c === "rock") {
-      return "2";
-    } else {
-      return "1";
-    }
-  }
 }
 //Function to do executed after gettin both choices
 function result(roomID) {
-  var winner = getWinner(choice1, choice2);
-  io.sockets.to(roomID).emit("result", {
-    winner: winner,
-    choice1: choice1,
-    choice2: choice2
-  });
-  choice1 = "";
-  choice2 = "";
+    var winner = getWinner(choice1, choice2);
+    io.sockets.to(roomID).emit("result", {
+        winner: winner,
+        choice1: choice1,
+        choice2: choice2
+    });
+    choice1 = "";
+    choice2 = "";
 }
 //Socket Connection
 io.on("connection", function(socket) {
-  console.log("made connection with socket");
+    console.log("made connection with socket");
 
-  //Disconnect
-  socket.on("disconnect", function(data) {
-    console.log("closed");
-    io.of("/")
-      .in(data.room)
-      .clients((error, socketIds) => {
-        if (error) throw error;
-        socketIds.forEach(socketId =>
-          io.sockets.sockets[socketId].leave("chat")
-        );
-      });
-  });
-
-  //Create Game Listener
-  socket.on("createGame", function(data) {
-    var rooms = randomstring.generate({
-      length: 4
+    //Disconnect
+    socket.on("disconnect", function(data) {
+        console.log("closed");
+        io.of("/")
+            .in(data.room)
+            .clients((error, socketIds) => {
+                if (error) throw error;
+                socketIds.forEach(socketId =>
+                    io.sockets.sockets[socketId].leave("chat")
+                );
+            });
     });
-    socket.join(rooms);
-    socket.emit("newGame", {
-      name: data.name,
-      room: rooms
-    });
-  });
-  //Join Game Listener
-  socket.on("joinGame", function(data) {
-    var room = io.nsps["/"].adapter.rooms[data.room];
-    if (room) {
-      if (room.length == 1) {
-        socket.join(data.room);
-        socket.broadcast.to(data.room).emit("player1", { oppName: data.name });
-        socket.emit("player2", { name: data.name, room: data.room });
-      } else {
-        socket.emit("err", { message: "Sorry, The room is full!" });
-      }
-    } else {
-      socket.emit("err", { message: "Invalid Room Key" });
-    }
-  });
-  //Listener to pass the name of the game creater
-  socket.on("joinedGame", function(data) {
-    console.log("Joined Game ", data);
-    socket.broadcast.to(data.room).emit("welcomeGame", data.player);
-  });
-  //Listener to Player 1's Choice
-  socket.on("choice1", function(data) {
-    choice1 = data.choice;
-    if (choice2 != "") {
-      result(data.room);
-    }
-  });
-  //Listener to Player 2's Choice
-  socket.on("choice2", function(data) {
-    choice2 = data.choice;
-    if (choice1 != "") {
-      result(data.room);
-    }
-  });
 
-  //Listener to Chat Messages
-  socket.on("chat", function(data) {
-    io.sockets.to(data.room).emit("chat", data);
-  });
-  socket.on("typing", function(data) {
-    socket.broadcast.to(data.room).emit("typing", data.player);
-  });
+    //Create Game Listener
+    socket.on("createGame", function(data) {
+        var rooms = randomstring.generate({
+            length: 4
+        });
+        socket.join(rooms);
+        socket.emit("newGame", {
+            name: data.name,
+            room: rooms
+        });
+    });
+    //Join Game Listener
+    socket.on("joinGame", function(data) {
+        var room = io.nsps["/"].adapter.rooms[data.room];
+        if (room) {
+            if (room.length == 1) {
+                socket.join(data.room);
+                socket.broadcast.to(data.room).emit("player1", { oppName: data.name });
+                socket.emit("player2", { name: data.name, room: data.room });
+            } else {
+                socket.emit("err", { message: "Sorry, The room is full!" });
+            }
+        } else {
+            socket.emit("err", { message: "Invalid Room Key" });
+        }
+    });
+    //Listener to pass the name of the game creater
+    socket.on("joinedGame", function(data) {
+        console.log("Joined Game ", data);
+        socket.broadcast.to(data.room).emit("welcomeGame", data.player);
+    });
+    //Listener to Player 1's Choice
+    socket.on("choice1", function(data) {
+        choice1 = data.choice;
+        console.log(choice1, choice2);
+        if (choice2 != "") {
+            result(data.room);
+        }
+    });
+    //Listener to Player 2's Choice
+    socket.on("choice2", function(data) {
+        choice2 = data.choice;
+        console.log(choice1, choice2);
+        if (choice1 != "") {
+            result(data.room);
+        }
+    });
+
+    //Listener to Chat Messages
+    socket.on("chat", function(data) {
+        io.sockets.to(data.room).emit("chat", data);
+    });
+    socket.on("typing", function(data) {
+        socket.broadcast.to(data.room).emit("typing", data.player);
+    });
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 //MAKE CONNECTION TO THE SOCKET
-var socket = io.connect("http://rpsgames.herokuapp.com");
+var socket = io.connect("http://localhost:3000");
+//var socket = io.connect("http://rpsgames.herokuapp.com");
 
 //VARIABLES AND CONSTANTS
-let playerChoice;
+let playerChoice = "";
 let opponentChoice;
 let winner;
 let roomID;
@@ -20,155 +21,164 @@ const result = document.getElementById("result");
 //const restart = document.getElementById("restart");
 const modal = document.querySelector(".modal");
 const scoreboard = {
-  player1: 0,
-  player2: 0
+    player1: 0,
+    player2: 0
 };
 
 //EMIT EVENTS
 
 //Chat Event Listener
 btn.addEventListener("click", function() {
-  socket.emit("chat", {
-    message: message.value,
-    handle: playerName,
-    room: roomID
-  });
+    socket.emit("chat", {
+        message: message.value,
+        handle: playerName,
+        room: roomID
+    });
 });
 //Typing Event Listener
 message.addEventListener("keypress", function() {
-  socket.emit("typing", {
-    room: roomID,
-    player: playerName
-  });
+    socket.emit("typing", {
+        room: roomID,
+        player: playerName
+    });
+});
+// Clear the Message
+btn.addEventListener("click", () => {
+    message.value = "";
 });
 
 //GAME EVENTS
 
 function disconnect() {
-  socket.emit("disconnect", {
-    room: roomID
-  });
+    socket.emit("disconnect", {
+        room: roomID
+    });
 }
 
 //Create New Game Listener
 $("#new").on("click", function() {
-  playerType = true;
-  playerName = $("#nameNew").val();
-  $("#player1Name").html(playerName);
-  if (!playerName) {
-    alert("Please enter your name.");
-    return;
-  }
-  socket.emit("createGame", { name: name });
-  $(".menu").fadeOut();
-  $(".gameBoard").fadeIn();
+    playerType = true;
+    playerName = $("#nameNew").val();
+    $("#player1Name").html(playerName);
+    if (!playerName) {
+        alert("Please enter your name.");
+        return;
+    }
+    socket.emit("createGame", { name: name });
+    $(".menu").fadeOut();
+    $(".gameBoard").fadeIn();
 });
 //Join Game Listener
 $("#join").on("click", function() {
-  playerType = false;
-  var name = $("#nameJoin").val();
-  playerName = name;
-  $("#player2Name").html(name);
-  roomID = $("#room").val();
-  if (!name || !roomID) {
-    alert("Please enter your name and game ID.");
-    return;
-  }
-  socket.emit("joinGame", { name: name, room: roomID });
-  $(".menu").fadeOut();
-  $(".gameBoard").fadeIn();
+    playerType = false;
+    var name = $("#nameJoin").val();
+    playerName = name;
+    $("#player2Name").html(name);
+    roomID = $("#room").val();
+    if (!name || !roomID) {
+        alert("Please enter your name and game ID.");
+        return;
+    }
+    socket.emit("joinGame", { name: name, room: roomID });
+    $(".menu").fadeOut();
+    $(".gameBoard").fadeIn();
 });
 
 //UI UPDATE EVENTS
 
 //New Game Welcome Wait Event Listener
 socket.on("newGame", function(data) {
-  var message =
-    "Hello, " +
-    data.name +
-    ". Please ask your friend to enter Game ID: " +
-    data.room +
-    ". Waiting for player 2...";
-  roomID = data.room;
-  $("#msg").html(message);
+    var message =
+        "Hello, " +
+        data.name +
+        ". Please ask your friend to enter Game ID: " +
+        data.room +
+        ". Waiting for player 2...";
+    roomID = data.room;
+    $("#msg").html(message);
 });
 //Player1 Joined Game Listener
 socket.on("player1", function(data) {
-  var message = "Hello , " + playerName;
-  $("#msg").html(message);
-  $("#player2Name").html(data.oppName);
-  socket.emit("joinedGame", {
-    room: roomID,
-    player: playerName
-  });
-  $(".gamePlay").css("display", "block");
+    var message = "Hello , " + playerName;
+    $("#msg").html(message);
+    $("#player2Name").html(data.oppName);
+    socket.emit("joinedGame", {
+        room: roomID,
+        player: playerName
+    });
+    $(".gamePlay").css("display", "block");
 });
 //Player2 Joined Game Listener
 socket.on("player2", function(data) {
-  var message = "Hello , " + playerName;
-  $("#msg").html(message);
+    var message = "Hello , " + playerName;
+    $("#msg").html(message);
 });
 //Update the Creater's Name Listener
 socket.on("welcomeGame", function(data) {
-  $("#player1Name").html(data);
-  $(".gamePlay").css("display", "block");
+    $("#player1Name").html(data);
+    $(".gamePlay").css("display", "block");
 });
 //Error Listener
 socket.on("err", function(err) {
-  alert(err.message);
-  location.reload();
+    alert(err.message);
+    location.reload();
 });
 //Result Listener
 socket.on("result", function(data) {
-  if (playerType) {
-    showWinner(data.winner, data.choice2);
-  } else {
-    showWinner(data.winner, data.choice1);
-  }
+    if (playerType) {
+        showWinner(data.winner, data.choice2);
+    } else {
+        showWinner(data.winner, data.choice1);
+    }
+    $("#" + playerChoice).css("color", "#333");
+    playerChoice = "";
 });
 
 //CHAT EVENTS
 
 //Listening to incoming message
 socket.on("chat", function(data) {
-  feedback.innerHTML = "";
-  output.innerHTML +=
-    "<p><strong>" + data.handle + "</strong> : " + data.message + "</p>";
+    feedback.innerHTML = "";
+    output.innerHTML +=
+        "<p><strong>" + data.handle + "</strong> : " + data.message + "</p>";
 });
 //Listening to typing
 socket.on("typing", function(data) {
-  feedback.innerHTML = data + " is typing a message";
+    feedback.innerHTML = data + " is typing a message";
 });
 
 // Play game
 function play(e) {
-  //restart.style.display = "inline-block";
-  playerChoice = e.target.id;
-  if (playerType) {
-    socket.emit("choice1", {
-      choice: playerChoice,
-      room: roomID
-    });
-  } else {
-    socket.emit("choice2", {
-      choice: playerChoice,
-      room: roomID
-    });
-  }
+    //restart.style.display = "inline-block";
+    if (playerChoice === "") {
+        playerChoice = e.target.id;
+        $("#" + e.target.id).css("color", "#c72121");
+        if (playerType) {
+            socket.emit("choice1", {
+                choice: playerChoice,
+                room: roomID
+            });
+        } else {
+            socket.emit("choice2", {
+                choice: playerChoice,
+                room: roomID
+            });
+        }
+    }
 }
 //Chat Open
 $(".chat").on("click", function() {
-  $("#players-chat").css("background", "rgba(0, 0, 0, 0.3)");
-  $("#players-chat").css("z-index", "1");
-  $("#chat-window").slideDown();
-  $(".chat").hide();
+    $("#players-chat").css("background", "rgba(0, 0, 0, 0.3)");
+    $("#players-chat").css("z-index", "1");
+    $("#chat-window").slideDown();
+    $(".chat").hide();
 });
 //Chat CLose
 $("#chatClose").on("click", function() {
-  $("#players-chat").css("background", "none");
-  $("#players-chat").css("z-index", "-1");
-  $("#chat-window").slideUp();
-  $(".chat").show();
+    $("#players-chat").css("background", "none");
+    $("#players-chat").css("z-index", "-1");
+    $("#chat-window").slideUp();
+    $(".chat").show();
 });
 // Get computers choice
 // function getComputerChoice() {
@@ -183,7 +193,7 @@ $("#chatClose").on("click", function() {
 // }
 
 function ResultDisplay(res, opponentChoice) {
-  result.innerHTML = `
+    result.innerHTML = `
       <h1 class="text-${res}">You ${res.charAt(0).toUpperCase() +
     res.slice(1)}</h1>
       <i class="fas fa-hand-${opponentChoice} fa-10x"></i>
@@ -191,37 +201,38 @@ function ResultDisplay(res, opponentChoice) {
         opponentChoice.slice(1)}</strong></p>
     `;
 }
+
 function showWinner(winner, opponentChoice) {
-  if (winner === "1") {
-    // Inc player score
-    scoreboard.player1++;
-    // Show modal result
-    if (playerType) {
-      ResultDisplay("win", opponentChoice);
+    if (winner === "1") {
+        // Inc player score
+        scoreboard.player1++;
+        // Show modal result
+        if (playerType) {
+            ResultDisplay("win", opponentChoice);
+        } else {
+            ResultDisplay("lose", opponentChoice);
+        }
+    } else if (winner === "2") {
+        // Inc computer score
+        scoreboard.player2++;
+        // Show modal result
+        if (!playerType) {
+            ResultDisplay("win", opponentChoice);
+        } else {
+            ResultDisplay("lose", opponentChoice);
+        }
     } else {
-      ResultDisplay("lose", opponentChoice);
-    }
-  } else if (winner === "2") {
-    // Inc computer score
-    scoreboard.player2++;
-    // Show modal result
-    if (!playerType) {
-      ResultDisplay("win", opponentChoice);
-    } else {
-      ResultDisplay("lose", opponentChoice);
-    }
-  } else {
-    result.innerHTML = `
+        result.innerHTML = `
       <h1>It's A Draw</h1>
       <i class="fas fa-hand-${opponentChoice} fa-10x"></i>
       o
     `;
-  }
-  // Show score
-  $("#score #p1").html(scoreboard.player1);
-  $("#score #p2").html(scoreboard.player2);
+    }
+    // Show score
+    $("#score #p1").html(scoreboard.player1);
+    $("#score #p2").html(scoreboard.player2);
 
-  modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 // Restart game
@@ -236,9 +247,9 @@ function showWinner(winner, opponentChoice) {
  */
 // Clear modal
 function clearModal(e) {
-  if (e.target == modal) {
-    modal.style.display = "none";
-  }
+    if (e.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 // Event listeners
